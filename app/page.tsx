ee12ab00c9176e8bd0d0e6acb2939b45757ebe89
Anna Viklund <annamariaviklund@gmail.com>
2025-09-08 10:27:36 +0200

    Setup Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-    return <h1>Antenna Go Admin</h1>;
+import { createClient } from '../utils/supabase/server';
+
+export default async function Page() {
+    const supabase = await createClient();
+    const { data: taxaLists } = await supabase.from('taxa_lists').select();
+
+    return (
+        <div>
+            <h1 className="mb-8">Antenna Go Admin</h1>
+            <pre>{JSON.stringify(taxaLists, null, 2)}</pre>
+        </div>
+    );
 }
