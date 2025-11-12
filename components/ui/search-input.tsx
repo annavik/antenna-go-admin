5a19ecc83f0683f2b7ea8b5e0873de2ab6cc4324
Anna Viklund <annamariaviklund@gmail.com>
2025-11-12 22:36:35 +0100

    Add clear button to search input

--- components/ui/search-input.tsx
+++ components/ui/search-input.tsx
@@ -1,5 +1,6 @@
-import { Loader2Icon, SearchIcon } from 'lucide-react';
+import { Loader2Icon, SearchIcon, XIcon } from 'lucide-react';
 import { Input } from './input';
+import { Button, buttonVariants } from './button';
 
 export const SearchInput = ({
     isLoading,
@@ -11,12 +12,18 @@ export const SearchInput = ({
     value: string;
 }) => (
     <div className="relative">
-        <div className="h-full flex items-center px-3 absolute top-0 left-0">
+        <div className="w-10 h-full flex items-center justify-center absolute top-0 left-0">
             <SearchIcon className="w-4 h-4" />
         </div>
         <Input className="px-10" value={value} onChange={(e) => onValueChange(e.currentTarget.value)} />
-        <div className="h-full flex items-center px-3 absolute top-0 right-0">
-            {isLoading ? <Loader2Icon className="w-4 h-4 text-secondary animate-spin" /> : null}
+        <div className="w-10 h-full flex items-center justify-center absolute top-0 right-0">
+            {isLoading ? (
+                <Loader2Icon className="w-4 h-4 text-secondary animate-spin" />
+            ) : value.length ? (
+                <Button onClick={() => onValueChange('')} size="icon" variant="ghost">
+                    <XIcon />
+                </Button>
+            ) : null}
         </div>
     </div>
 );
