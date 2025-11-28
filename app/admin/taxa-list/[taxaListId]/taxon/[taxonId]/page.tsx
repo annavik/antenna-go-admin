f78daf86fe5f4327b7da234d28d2559ad4206fec
Anna Viklund <annamariaviklund@gmail.com>
2025-11-28 23:06:45 +0100

    Tweak error handling

--- app/admin/taxa-list/[taxaListId]/taxon/[taxonId]/page.tsx
+++ app/admin/taxa-list/[taxaListId]/taxon/[taxonId]/page.tsx
@@ -6,12 +6,12 @@ export default async function Page({ params }) {
     const { taxonId, taxaListId } = await params;
     const supabase = await createClient();
     const { data } = await supabase.from('taxa').select('*, tags( * )').eq('id', taxonId).maybeSingle();
-    const { tags: taxonTags, ...taxon } = data;
 
-    if (!taxon) {
+    if (!data) {
         return notFound();
     }
 
+    const { tags: taxonTags, ...taxon } = data;
     const { data: tags } = await supabase.from('tags').select().eq('taxa_list_id', taxaListId).order('created_at');
 
     return <TaxonForm taxaListId={taxaListId} taxaListTags={tags} taxon={taxon} taxonTags={taxonTags} />;
--- app/taxa-list/[taxaListId]/taxon/[taxonId]/page.tsx
+++ app/taxa-list/[taxaListId]/taxon/[taxonId]/page.tsx
@@ -7,6 +7,11 @@ export default async function Page({ params }) {
     const { taxaListId, taxonId } = await params;
     const supabase = await createClient();
     const { data: taxaList } = await supabase.from('taxa_lists').select().eq('id', taxaListId).maybeSingle();
+
+    if (!taxaList) {
+        return notFound();
+    }
+
     const { data } = await supabase.from('taxa').select('*, tags( * )').eq('id', taxonId).maybeSingle();
 
     if (!data) {
