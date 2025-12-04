f628b6036a192db58badc819e57311e25aa613a5
Anna Viklund <annamariaviklund@gmail.com>
2025-12-04 14:27:10 +0100

    Add search feature to taxa

--- components/taxa-lists/taxa-list.tsx
+++ components/taxa-lists/taxa-list.tsx
@@ -4,11 +4,18 @@ import { TaxaGallery } from '@/components/taxa/taxa-gallery';
 import { TaxaTable } from '@/components/taxa/taxa-table';
 import { Tables } from '@/lib/supabase/database.types';
 import { TaxonDetails } from '@/lib/types';
+import Fuse from 'fuse.js';
 import { Grid2X2Icon, TableIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { ExportTaxa } from '../taxa/export-taxa';
 import { ButtonTooltip } from '../ui/button-tooltip';
+import { SearchInput } from '../ui/search-input';
 import { ToggleGroup, ToggleGroupItem } from '../ui/toggle-group';
+
+const SEARCH_OPTIONS = {
+    keys: ['label', 'common_name'],
+    threshold: 0.4
+};
 
 const VIEW_MODES = [
     {
@@ -25,6 +32,9 @@ const VIEW_MODES = [
 
 export const TaxaList = ({ taxa, taxaList }: { taxa: TaxonDetails[]; taxaList: Tables<'taxa_lists'> }) => {
     const [viewMode, setViewMode] = useState('gallery');
+    const [searchString, setSearchString] = useState('');
+    const fuse = useMemo(() => new Fuse(taxa, SEARCH_OPTIONS), [taxa]);
+    const filteredTaxa = searchString.length ? fuse.search(searchString)?.map(({ item }) => item) : taxa;
 
     return (
         <>
@@ -34,20 +44,27 @@ export const TaxaList = ({ taxa, taxaList }: { taxa: TaxonDetails[]; taxaList: T
                     {taxaList.description ? <span className="body-base">{taxaList.description}</span> : null}
                 </div>
                 <div className="flex items-end justify-end gap-4">
-                    <ToggleGroup type="single" value={viewMode} onValueChange={setViewMode}>
-                        {VIEW_MODES.map(({ Icon, tooltip, value }) => (
-                            <ButtonTooltip key={value} content={tooltip}>
-                                <ToggleGroupItem isActive={value === viewMode} value={value}>
-                                    <Icon className="w-4 h-4" />
-                                </ToggleGroupItem>
-                            </ButtonTooltip>
-                        ))}
-                    </ToggleGroup>
-                    {taxa.length ? <ExportTaxa taxaListId={taxaList.id} /> : null}
+                    <div className="flex items-center gap-4">
+                        <ToggleGroup type="single" value={viewMode} onValueChange={setViewMode}>
+                            {VIEW_MODES.map(({ Icon, tooltip, value }) => (
+                                <ButtonTooltip key={value} content={tooltip}>
+                                    <ToggleGroupItem isActive={value === viewMode} value={value}>
+                                        <Icon className="w-4 h-4" />
+                                    </ToggleGroupItem>
+                                </ButtonTooltip>
+                            ))}
+                        </ToggleGroup>
+                        <SearchInput
+                            placeholder="Search taxa..."
+                            onValueChange={setSearchString}
+                            value={searchString}
+                        />
+                        {taxa.length ? <ExportTaxa taxaListId={taxaList.id} /> : null}
+                    </div>
                 </div>
             </div>
-            {viewMode === 'gallery' ? <TaxaGallery taxa={taxa} /> : null}
-            {viewMode === 'table' ? <TaxaTable taxa={taxa} /> : null}
+            {viewMode === 'gallery' ? <TaxaGallery taxa={filteredTaxa} /> : null}
+            {viewMode === 'table' ? <TaxaTable taxa={filteredTaxa} /> : null}
         </>
     );
 };
